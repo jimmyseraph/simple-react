419434bd74fcd8278eb54dc7a59a041c00d32d22
liudao <jimmyseraph@163.com>
2019-10-22 16:30:38 +0800

    fix bug

--- src/Prime.js
+++ src/Prime.js
@@ -8,7 +8,7 @@ class Prime extends Component {
 
     isPrime = num => {
         for(let i = 2; i <= Math.sqrt(num); i++){
-            if(num % i == 0){
+            if(num % i === 0){
                 return false;
             }
         }
